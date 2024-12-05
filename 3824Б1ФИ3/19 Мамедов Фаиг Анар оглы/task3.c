5bc3f8605b4e3ef085992b9fd563ab61095a07a0
fvur <86082004+Dyx77@users.noreply.github.com>
2024-12-05 10:22:26 +0300

    Update task3.c

--- 3824Б1ФИ3/19 Мамедов Фаиг Анар оглы/task3.c
+++ 3824Б1ФИ3/19 Мамедов Фаиг Анар оглы/task3.c
@@ -1,5 +1,17 @@
 // Задача 3. Найти максимум из вешественных чисел a и b
+// немного не понял ввод должен быть от пользователя или можно на конкретных числах
+
 double max_of_two_numbers(double a, double b)
 {
-  return 0.0;
+    return (a > b) ? a : b;
+}
+
+int main() {
+    double num1, num2;
+    printf("Введите первое вещественное число: ");
+    scanf("%lf", &num1);
+    printf("Введите второе вещественное число: ");
+    scanf("%lf", &num2);
+    printf("Максимум из %.2f и %.2f: %.2f\n", num1, num2, max_of_two_numbers(num1, num2));
+    return 0;
 }
