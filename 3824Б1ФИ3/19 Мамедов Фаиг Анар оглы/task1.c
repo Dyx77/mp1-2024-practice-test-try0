aa83cf09f894d385846b499aa72a50e12d4f5301
fvur <86082004+Dyx77@users.noreply.github.com>
2024-12-05 10:27:16 +0300

    Update task1.c

--- 3824Б1ФИ3/19 Мамедов Фаиг Анар оглы/task1.c
+++ 3824Б1ФИ3/19 Мамедов Фаиг Анар оглы/task1.c
@@ -3,5 +3,14 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+    if (a > 0 && b > INT_MAX - a) return INT_MAX;
+    if (a < 0 && b < INT_MIN - a) return INT_MIN;
+    return a + b;
+}
+
+int main() {
+    int a = 10, b = 20;
+    int sum = sum_of_two_numbers(a, b);
+    printf("Сумма: %d\n", sum);
+    return 0;
 }
