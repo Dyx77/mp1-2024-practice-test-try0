dfe84b64e329e813e3e9b4fda19659758f96c0dd
fvur <86082004+Dyx77@users.noreply.github.com>
2024-12-05 10:35:00 +0300

    Update task3.c

--- 3824Б1ФИ3/19 Мамедов Фаиг Анар оглы/task3.c
+++ 3824Б1ФИ3/19 Мамедов Фаиг Анар оглы/task3.c
@@ -2,4 +2,20 @@
 double max_of_two_numbers(double a, double b)
 {
   return 0.0;
+  return (a > b) ? a : b;
+}
+
+int main() {
+    double num1, num2;
+
+    // Ввод чисел от пользователя
+    printf("Введите первое вещественное число: ");
+    scanf("%lf", &num1);
+    printf("Введите второе вещественное число: ");
+    scanf("%lf", &num2);
+
+    // Вывод максимального числа
+    printf("Максимум из %.2f и %.2f: %.2f\n", num1, num2, max_of_two_numbers(num1, num2));
+
+    return 0;
 }
